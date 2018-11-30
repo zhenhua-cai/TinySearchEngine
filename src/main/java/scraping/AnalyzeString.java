e65b6df0220df020c6b7b90c280ce1f52ece79a3
Zhenhua Cai <steven.cai1990@hotmail.com>
2018-11-30 18:20:34 -0500

    add AnalyzeString Method

--- src/main/java/scraping/AnalyzeString.java
+++ src/main/java/scraping/AnalyzeString.java
@@ -1,8 +1,15 @@
 package scraping;
 
 public class AnalyzeString {
+    /**
+     * get the paragraph that contains the keyword in the str.
+     * @param str the str to be analyzed
+     * @param keyword the keyword
+     * @return the final paragraph that contains the keyword.
+     */
     public static String getPara(String str,String keyword){
-        
+
+
         return null;
     }
 }
