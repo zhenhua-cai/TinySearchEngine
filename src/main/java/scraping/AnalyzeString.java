9ace97906e5deb6edca9feec7fd8f0bafa8da292
woshizuoyue <36312959+woshizuoyue@users.noreply.github.com>
2018-12-01 17:36:29 -0500

    Update AnalyzeString.java

--- src/main/java/scraping/AnalyzeString.java
+++ src/main/java/scraping/AnalyzeString.java
@@ -10,6 +10,69 @@ public class AnalyzeString {
     public static String getPara(String str,String keyword){
 
 
-        return null;
+       
+   String hold = "";
+        
+       hold = test1(str, keyword);
+        
+        return hold;
+    }
+    
+    public static String test1(String str, String keyword)
+    {
+    	
+    	String hold1 = "";
+    	
+    	int stringHead, stringTail;
+
+        if(!str.contains(keyword))
+
+            hold1 = "There is no keyword in the analyzed paragraph";
+
+        else
+        {
+        	
+        	stringHead = str.indexOf(keyword);
+        	
+        	System.out.println(stringHead+"@@");
+        	
+        	
+        	stringTail = stringHead + keyword.length();
+        	
+        	System.out.println(stringTail+"##");
+        	
+        	System.out.println(str.length()+"$$");
+        	
+        	
+        	if(stringHead==0)
+        	{
+        		
+        		hold1 = str.substring(stringHead, stringTail+20);
+        		
+        		System.out.println("head shows");
+        		
+        	}
+        	
+        	if(stringTail == str.length())
+        	{
+        		
+        		hold1 = str.substring(stringHead-20);// need correct;
+        		
+        		System.out.println("tail shows");
+        	
+        	}
+        	
+        	if(stringHead != 0 && stringTail!=str.length())
+        	{
+        		
+        		hold1 = str.substring(stringHead-10, stringTail+10);
+        		
+        	}
+        	
+        	
+        		
+        }
+        
+        return hold1;
     }
 }
